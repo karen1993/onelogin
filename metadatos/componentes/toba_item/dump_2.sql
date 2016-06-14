047d9bd9efcfeb85d7e90935232afad7345c9139
dtroncoso <davidariel.troncoso@gmail.com>
2016-06-14 12:09:50 -0300

    conExportar

--- metadatos/componentes/toba_item/dump_2.sql
+++ metadatos/componentes/toba_item/dump_2.sql
@@ -18,18 +18,18 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	'0', --nivel_acceso
 	'web', --solicitud_tipo
 	'toba', --pagina_tipo_proyecto
-	'normal', --pagina_tipo
+	'vacio', --pagina_tipo
 	'toba', --actividad_buffer_proyecto
 	'0', --actividad_buffer
 	'toba', --actividad_patron_proyecto
 	'especifico', --actividad_patron
 	'Inicio', --nombre
 	'', --descripcion
-	NULL, --punto_montaje
+	'1000004', --punto_montaje
 	'item_inicial.php', --actividad_accion
 	'1', --menu
 	'0', --orden
-	NULL, --solicitud_registrar
+	'0', --solicitud_registrar
 	NULL, --solicitud_obs_tipo_proyecto
 	NULL, --solicitud_obs_tipo
 	NULL, --solicitud_observacion
@@ -38,16 +38,16 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	NULL, --zona_proyecto
 	NULL, --zona
 	NULL, --zona_orden
-	NULL, --zona_listar
-	NULL, --imagen_recurso_origen
+	'0', --zona_listar
+	'apex', --imagen_recurso_origen
 	NULL, --imagen
 	NULL, --parametro_a
 	NULL, --parametro_b
 	NULL, --parametro_c
-	NULL, --publico
+	'1', --publico
 	NULL, --redirecciona
 	NULL, --usuario
-	NULL, --exportable
+	'0', --exportable
 	'2016-05-31 10:51:45', --creacion
 	'0'  --retrasar_headers
 );
